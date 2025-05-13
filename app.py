6b2289da63404aed4b987464b2ce4dd61d75717e
kehindealawiye <128984208+kehindealawiye@users.noreply.github.com>
2025-05-13 12:38:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -316,16 +316,6 @@ if st.button("Generate Pivot Table"):
 # === Section: Export PDF Summary (Filtered View) ===
 st.subheader("Export PDF Summary")
 
-class PDF(FPDF):
-    def header(self):
-        try:
-            self.image("Lagos-logo.png", x=10, y=8, w=25)
-        except:
-            pass
-
-def encode_latin(text):
-    return text.encode("latin-1", "ignore").decode("latin-1")
-
 if st.button("Download PDF Summary"):
     with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmpfile:
         pdf = PDF(orientation="L")
@@ -393,6 +383,3 @@ if st.button("Download PDF Summary"):
         pdf.output(tmpfile.name)
         with open(tmpfile.name, "rb") as f:
             st.download_button("Download PDF", f, file_name=f"PMR_Summary_{quarter}_{year}.pdf")
-
-
-            
